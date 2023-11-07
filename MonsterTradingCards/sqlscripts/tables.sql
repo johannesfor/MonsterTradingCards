b29b18c836ad0bf5ee992d3cdf8519e85ebc2594
johannesforstandlechner <johannesforstandlechner@gmail.com>
2023-11-07 12:42:55 +0100

    removed the relations tables and moved the relations to the card table

--- MonsterTradingCards/sqlscripts/tables.sql
+++ MonsterTradingCards/sqlscripts/tables.sql
@@ -1,54 +1,46 @@
 ﻿use monstertradingcards;
 
+DROP TABLE IF EXISTS marketplace;
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS package;
+
 create table "user" (
-	id UUID,
+	id UUID primary key,
 	username varchar(255),
 	password varchar(255),
-	profile_description varchar(255),
+	bio varchar(255),
+	image varchar(255),
 	coins number,
-	deck_id UUID,
-	stack_id UUID,
 	elo number,
 	played_games number,
-	//foreign keys
-)
-
-create table deck (
-	id UUID,
-	card_id number,
-)
-
-create table stack (
-	id UUID,
-	card_id UUID,
-
-	//foreign keys
 )
 
 create table card (
-	id UUID,
+	id UUID primary key,
 	name varchar(255),
 	damage number,
 	element_type number,
 	card_type number,
 
-)
+	user_id UUID,
+	is_in_deck bool,
 
-create table package_cards (
-	id UUID,
 	package_id UUID,
-	card_id UUID,
 
-	//foreign keys
+	CONSTRAINT fk_card_user FOREIGN KEY (user_id) REFERENCES "user"(id),
+	CONSTRAINT fk_card_package FOREIGN KEY (package_id) REFERENCES package(id)
 )
 
 create table package (
-	id UUID
+	id UUID primary key
 )
 
 create table marketplace (
-	id UUID,
+	id UUID primary key,
 	card_id UUID,
+	requirement_card_type number,
+	requirement_min_damage number
 
-	//foreign keys
+	CONSTRAINT fk_marketplace_card FOREIGN KEY (card_id) REFERENCES card(id)
 )
