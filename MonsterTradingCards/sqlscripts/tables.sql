bee8fdf2ef61aad999377ac8387f465639496efe
Johannes Forstandlechner <johannes.forstandlechner@ace.at>
2023-11-07 13:05:40 +0100

    Adapted the sql scheme so now it works with postgres

--- MonsterTradingCards/sqlscripts/tables.sql
+++ MonsterTradingCards/sqlscripts/tables.sql
@@ -1,46 +1,46 @@
-﻿use monstertradingcards;
+﻿\c monster_trading_cards
 
 DROP TABLE IF EXISTS marketplace;
 DROP TABLE IF EXISTS card;
-DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS package;
 
-create table "user" (
+create table users (
 	id UUID primary key,
 	username varchar(255),
 	password varchar(255),
 	bio varchar(255),
 	image varchar(255),
-	coins number,
-	elo number,
-	played_games number,
-)
+	coins integer,
+	elo integer,
+	played_games integer
+);
+
+create table package (
+	id UUID primary key
+);
 
 create table card (
 	id UUID primary key,
 	name varchar(255),
-	damage number,
-	element_type number,
-	card_type number,
+	damage integer,
+	element_type integer,
+	card_type integer,
 
 	user_id UUID,
 	is_in_deck bool,
 
 	package_id UUID,
 
-	CONSTRAINT fk_card_user FOREIGN KEY (user_id) REFERENCES "user"(id),
+	CONSTRAINT fk_card_user FOREIGN KEY (user_id) REFERENCES users(id),
 	CONSTRAINT fk_card_package FOREIGN KEY (package_id) REFERENCES package(id)
-)
-
-create table package (
-	id UUID primary key
-)
+);
 
 create table marketplace (
 	id UUID primary key,
 	card_id UUID,
-	requirement_card_type number,
-	requirement_min_damage number
+	requirement_card_type integer,
+	requirement_min_damage integer,
 
 	CONSTRAINT fk_marketplace_card FOREIGN KEY (card_id) REFERENCES card(id)
-)
+);
