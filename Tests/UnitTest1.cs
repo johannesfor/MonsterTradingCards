cf824a7fe1a2a08963401099c978fa9700725c96
johannesforstandlechner <johannesforstandlechner@gmail.com>
2023-11-29 14:31:52 +0100

    Added the unit test project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using MonsterTradingCards;
+using MonsterTradingCards.CAQ.Battle;
+using MonsterTradingCards.Handler.Battle;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBattleLogic()
+        {
+            new BattleWithRandomPlayerCommand();
+
+            
+        }
+    }
+}
